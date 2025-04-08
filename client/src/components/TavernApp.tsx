ff2163c923b3efe2dba6192068b3caf036c73212
truwubs <39683752-truwubs@users.noreply.replit.com>
2025-04-08 07:22:31 +0000

    Restored to '015620ebfe87553856ce643194700c45c972fe62'
    Replit-Restored-To: 015620ebfe87553856ce643194700c45c972fe62

--- client/src/components/TavernApp.tsx
+++ client/src/components/TavernApp.tsx
@@ -4,7 +4,6 @@ import ChatPanel from './ChatPanel';
 import OnlineUsers from './OnlineUsers';
 import CharacterSelection from './CharacterSelection';
 import TavernMenu from './TavernMenu';
-import TavernAudio from './TavernAudio';
 import { useWebSocketStore } from '@/lib/websocket';
 import { Menu } from 'lucide-react';
 import { Button } from '@/components/ui/button';
@@ -82,9 +81,6 @@ const TavernApp: FC = () => {
       
       {/* Menu */}
       {showMenu && <TavernMenu onClose={toggleMenu} />}
-      
-      {/* Audio System - This remains mounted all the time */}
-      <TavernAudio />
     </div>
   );
 };
